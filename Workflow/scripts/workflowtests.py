f9d3b390aceb1bde0c6978f404bda6f3012e0dc6
Stephane Poss <stephposs@gmail.com>
2013-04-30 11:27:29 +0200

    CHANGE: the inastances ARE needed

--- Workflow/scripts/workflowtests.py
+++ Workflow/scripts/workflowtests.py
@@ -18,13 +18,14 @@ moduledefinition.addParameter(Parameter('something', '12', 'string',  "", "", Fa
 stepdefinition = StepDefinition("step_1")
 stepdefinition.addModule(moduledefinition)
 
-m1i = stepdefinition.createModuleInstance(moduledefinition.getType(), stepdefinition.getType())#This is optional
+m1i = stepdefinition.createModuleInstance(moduledefinition.getType(), stepdefinition.getType())#This is needed!
+#It's what creates the instance, it goes beyond definition
 m1i.setValue('something', '14')#This is optional
 
 wo = Workflow(name = 'a_test')
 wo.addStep(stepdefinition)
-stepInstance = wo.createStepInstance(stepdefinition.getType(), "step_1") #In fact this is not needed, but useful if there
-#are some step parameters
+stepInstance = wo.createStepInstance(stepdefinition.getType(), "step_1") #In fact this is used to create the instance
+#so it is needed
 
 #add a parameter to the global workflow, just because we can
 wo.addParameter(Parameter("MyParam","SomeParam","string","","",False,  False, "a  workflow parameter"))
